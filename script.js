46d382364c3e31b3a562d54cbe4b34b96a23243e
vovamoskvych-source <vovamoskvych@gmail.com>
2026-04-11 23:03:56 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,77 @@
-alert("JS ЗАПУЩЕН");
-
+const main = document.getElementById("main");
+const gameScreen = document.getElementById("gameScreen");
 const ball = document.getElementById("ball");
+const scoreEl = document.getElementById("score");
 
 let ballY = 150;
 let velocity = 0;
 
 const gravity = 0.8;
-const jumpPower = -10;
+const jumpPower = -11;
 
-let playing = true;
+let playing = false;
+let loopStarted = false;
 
-/* 🔥 ПРЫЖОК */
-function jump() {
-    alert("ПРЫЖОК СРАБОТАЛ"); // <--- ВАЖНО
+let score = 0;
 
-    velocity = jumpPower;
+/* OPEN */
+function openGame() {
+    main.style.display = "none";
+    gameScreen.style.display = "block";
+
+    reset();
 }
 
-/* 🔥 СОБЫТИЕ НА ВСЮ СТРАНИЦУ */
-document.body.addEventListener("click", jump);
-document.body.addEventListener("touchstart", jump);
+/* START */
+function startGame() {
+    document.getElementById("startScreen").style.display = "none";
+    playing = true;
+
+    if (!loopStarted) {
+        loopStarted = true;
+        requestAnimationFrame(loop);
+    }
+}
+
+/* RESET */
+function reset() {
+    ballY = 150;
+    velocity = 0;
+    score = 0;
+    playing = false;
+
+    ball.style.bottom = ballY + "px";
+    scoreEl.innerText = "0";
+
+    document.getElementById("startScreen").style.display = "flex";
+}
+
+/* JUMP */
+document.addEventListener("click", () => {
+    if (!playing) return;
+    velocity = jumpPower;
+});
 
 /* LOOP */
 function loop() {
+    if (!loopStarted) return;
 
-    velocity += gravity;
-    ballY += velocity;
+    if (playing) {
 
-    if (ballY < 0) {
-        ballY = 0;
-        velocity = 0;
+        velocity += gravity;
+        ballY += velocity;
+
+        // земля
+        if (ballY < 0) {
+            ballY = 0;
+            velocity = 0;
+        }
+
+        ball.style.bottom = ballY + "px";
+
+        score++;
+        scoreEl.innerText = score;
     }
-
-    ball.style.bottom = ballY + "px";
 
     requestAnimationFrame(loop);
 }
-
-loop();
